c4bdb7b9e5d992771dbd39b2daa563ed160095d9
Arne Van Raepenbusch <arne@avantika.be>
2026-07-24 09:48:17 +0200

    added exam questions

--- src/data/courses/klinpsy1a/data.ts
+++ src/data/courses/klinpsy1a/data.ts
@@ -26,8 +26,20 @@ import flashcardsH13_matthews_02 from '../../flashcards/klinpsy1a/flashcards_h13
 import flashcardsH13_matthews_03 from '../../flashcards/klinpsy1a/flashcards_h13_matthews_03.json'
 import flashcardsH14 from '../../flashcards/klinpsy1a/flashcards_h14.json'
 import flashcardsH15 from '../../flashcards/klinpsy1a/flashcards_h15.json'
-
 // --- Exams ---
+import examQuestionsH1Raw from '../../exams/klinpsy1a/exam_questions_h1.json'
+import examQuestionsH2Raw from '../../exams/klinpsy1a/exam_questions_h2.json'
+import examQuestionsH3Raw from '../../exams/klinpsy1a/exam_questions_h3.json'
+import examQuestionsH4Raw from '../../exams/klinpsy1a/exam_questions_h4.json'
+import examQuestionsH5Raw from '../../exams/klinpsy1a/exam_questions_h5.json'
+import examQuestionsH6Raw from '../../exams/klinpsy1a/exam_questions_h6.json'
+import examQuestionsH7Raw from '../../exams/klinpsy1a/exam_questions_h7.json'
+import examQuestionsH8Raw from '../../exams/klinpsy1a/exam_questions_h8.json'
+import examQuestionsH9Raw from '../../exams/klinpsy1a/exam_questions_h9.json'
+import examQuestionsH10Raw from '../../exams/klinpsy1a/exam_questions_h10.json'
+import examQuestionsH21Raw from '../../exams/klinpsy1a/exam_questions_h21.json'
+import examQuestionsH22Raw from '../../exams/klinpsy1a/exam_questions_h22.json'
+import examQuestionsH23Raw from '../../exams/klinpsy1a/exam_questions_h23.json'
 
 // --- Compendiums ---
 
@@ -69,7 +81,73 @@ const chapterNumbers = [1, 2, 3, 4, 5, 6, 7, 11, 12, 13, 14, 15] as const
 type RawExamQuestion = Omit<ExamQuestion, 'source'>
 type ExamSourceEntry = { source: string; label: string; questions: RawExamQuestion[] }
 
-const EXAM_SOURCE_ENTRIES: ExamSourceEntry[] = []
+const EXAM_SOURCE_ENTRIES: ExamSourceEntry[] = [
+  {
+    source: 'exam_questions_h1.json',
+    label: 'Examen vragen Hoofdstuk 1',
+    questions: examQuestionsH1Raw as RawExamQuestion[],
+  },
+  {
+    source: 'exam_questions_h2.json',
+    label: 'Examen vragen Hoofdstuk 2',
+    questions: examQuestionsH2Raw as RawExamQuestion[],
+  },
+  {
+    source: 'exam_questions_h3.json',
+    label: 'Examen vragen Hoofdstuk 3',
+    questions: examQuestionsH3Raw as RawExamQuestion[],
+  },
+  {
+    source: 'exam_questions_h4.json',
+    label: 'Examen vragen Hoofdstuk 4',
+    questions: examQuestionsH4Raw as RawExamQuestion[],
+  },
+  {
+    source: 'exam_questions_h5.json',
+    label: 'Examen vragen Hoofdstuk 5',
+    questions: examQuestionsH5Raw as RawExamQuestion[],
+  },
+  {
+    source: 'exam_questions_h6.json',
+    label: 'Examen vragen Hoofdstuk 6',
+    questions: examQuestionsH6Raw as RawExamQuestion[],
+  },
+  {
+    source: 'exam_questions_h7.json',
+    label: 'Examen vragen Hoofdstuk 7',
+    questions: examQuestionsH7Raw as RawExamQuestion[],
+  },
+  {
+    source: 'exam_questions_h8.json',
+    label: 'Examen vragen Hoofdstuk 8',
+    questions: examQuestionsH8Raw as RawExamQuestion[],
+  },
+  {
+    source: 'exam_questions_h9.json',
+    label: 'Examen vragen Hoofdstuk 9',
+    questions: examQuestionsH9Raw as RawExamQuestion[],
+  },
+  {
+    source: 'exam_questions_h10.json',
+    label: 'Examen vragen Hoofdstuk 10',
+    questions: examQuestionsH10Raw as RawExamQuestion[],
+  },
+  {
+    source: 'exam_questions_h21.json',
+    label: 'Examen vragen Hoofdstuk 21',
+    questions: examQuestionsH21Raw as RawExamQuestion[],
+  },
+  {
+    source: 'exam_questions_h22.json',
+    label: 'Examen vragen Hoofdstuk 22',
+    questions: examQuestionsH22Raw as RawExamQuestion[],
+  },
+  {
+    source: 'exam_questions_h23.json',
+    label: 'Examen vragen Hoofdstuk 23',
+    questions: examQuestionsH23Raw as RawExamQuestion[],
+  }
+]
 
 const exams: ExamQuestion[] = EXAM_SOURCE_ENTRIES.flatMap(({ source, questions }) =>
   questions.map((q) => ({ ...q, source })),
